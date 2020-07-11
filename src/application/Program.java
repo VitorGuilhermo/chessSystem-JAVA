bd9bd1720f08e0a6889e6a08a7495d6b6ea996c2
Vitor de Araujo Romão Guilhermo <viguilhermo@hotmail.com>
2020-07-11 20:54:20 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,15 @@
 package application;
 
+import boadgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 
-		System.out.println("Hello World!");
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+		
 	}
 
 }
